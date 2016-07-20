6f5584c9c58d93375057ef847dd262e1e9ee3df5
bryantbello <bryant.bello@outlook.com>
2016-07-19 23:33:55 -0400

    ruby obj

---
+++ objects.rb
@@ -0,0 +1,5 @@
+-1.abs                               # => 1
+'1234567'.length                     # => 7 
+1234567.to_s.length                  # => 7
+3.times {print 3}                    # outputs, 333
+rand(10).times { |x| puts x }        # => random set of numbers from 0 to 10
