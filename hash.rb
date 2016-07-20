cce88e4af523b3ec3ee5de8d620fe6a110986b9b
bryantbello <bryant.bello@outlook.com>
2016-07-19 23:32:49 -0400

    ruby hash

---
+++ hash.rb
@@ -0,0 +1,6 @@
+capitals = {                  
+:sindh  => 'Karachi',
+:punjab => 'Lahore'
+}
+capitals[:westbengal] = 'Kolkata'     # append a new element
+capitals[:karnataka] = 'Bengaluru'    # change an element's association
